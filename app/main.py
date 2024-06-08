c0348dcee51628bb27d273cff1637b04bc41790c
Ibinola <ibinolamichael1@gmail.com>
2024-06-08 22:39:23 +0100

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,20 +9,17 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection, response =  server_socket.accept() # wait for client
-    response[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    client_socket, client_address =  server_socket.accept() # wait for client
+    # client_address[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
-    while True:
-        request = connection.recv(1500).decode()
-        headers = request.split('\n')
-        first_header_components = headers[0].split()
+    response = client_socket.recv(1024).decode('utf-8')
+    # print(request)
+    path = response.split(" ")[1]
 
-        path = first_header_components[1]
-
-        if path == '/':
-            return 'HTTP/1.1 200 OK\r\n\r\n'
-        else:
-            return 'HTTP/1.1 404 Not Found\r\n\r\n'
+    if path == "/":
+        client_socket.send('HTTP/1.1 200 OK\r\n\r\n')
+    else:
+        client_socket.send('HTTP/1.1 404 Not Found\r\n\r\n')
 
     
 
